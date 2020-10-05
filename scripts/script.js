a39a0f11b593ff071ae572cf9b5c6d82b2fe0e64
Dominik Schick <dominikschick@outlook.com>
2020-10-05 11:47:58 +0200

    Updated script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,9 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
-alert('Hello World')
-openModalButtons.forEach(button => {
-  button.addEventListener('load', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
 
 overlay.addEventListener('load', () => {
+  alert('Hello World')
   const modals = document.querySelectorAll('.modal.active')
   modals.forEach(modal => {
     closeModal(modal)
